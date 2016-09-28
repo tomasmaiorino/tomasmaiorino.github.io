b378baf6b19e519b2ff1d92b16088b2f519b0b6c
tomasmaiorino <tomasmaiorino@gmail.com>
2016-09-27 22:22:08 -0300

    wip

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -9,7 +9,7 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -20,14 +20,18 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://boiling-stream-89308.herokuapp.com/api/v1/sendEmail",
                 type: "POST",
+                crossDomain: true,
+                dataType: "json",
                 data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+                    sender_name: name,
+                    sender_email: email,
+                    message: message,
+                    subject: 'Contact',
+                    token: '4bcwqzoi4gng4'
                 },
                 cache: false,
                 success: function() {
@@ -37,7 +41,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Message sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -49,12 +53,12 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", we've some problems :( .Please try again later");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-            });
+            })
         },
         filter: function() {
             return $(this).is(":visible");

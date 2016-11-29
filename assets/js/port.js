3b31ffe1ab5f9e37421bdce7a93a3c571e4febc0
tomasmaiorino <tomasmaiorino@gmail.com>
2016-11-29 14:56:05 -0200

    update change color method

--- assets/js/port.js
+++ assets/js/port.js
@@ -4,20 +4,40 @@ var SKILL_URL = "/api/v1/company/skill/";
 var DEFAULT_COLOR = "#5cb85c";
 var OWN_PROJECT_COLOR = "#3AAA64";
 
-var fieldsToChangeColor = [
-    {clazz : "email-link-link", attr: "color:"},
-    {clazz : "level-bar-inner", attr: "background:"},
-    {clazz : "tech-defaulf-content", attr: "color:"},
-    {clazz : "label-theme", attr: "background:"},
-    {clazz : "label-success", attr: "background-color:"},
-    {clazz : "more-link", attr: "color:"},
-    {clazz : "fa-star", attr: "color:"},
-    {clazz : "fa-star-half", attr: "color:"},
-    {clazz : "contact-scroll", attr: "border:1px solid #"}
-];
+var fieldsToChangeColor = [{
+  clazz: "email-link-link",
+  attr: "color:",
+}, {
+  clazz: "tech-defaulf-content",
+  attr: "color:"
+}, {
+  clazz: "label-theme",
+  attr: "background:"
+}, {
+  clazz: "label-success",
+  attr: "background-color:"
+}, {
+  clazz: "more-link",
+  attr: "color:"
+}, {
+  clazz: "fa-star",
+  attr: "color:"
+}, {
+  clazz: "fa-star-half",
+  attr: "color:"
+}, {
+  clazz: "contact-scroll",
+  attr: "border:1px solid #;background-color: #"
+}, {
+  clazz: "level-bar-inner",
+  attr: "background-color:"
+}];
 
+var skillLoadClass = [{
+  clazz: "level-bar-inner",
+  attr: "background-color:"
+}]
 //var fieldsToChangeColor = ['email-link', 'level-bar-inner', 'tech-defaulf-content', 'label-theme', 'more-link', 'fa fa-star'];
-
 app.factory("SkillService", function() {
   return {
     companySkills: function() {
@@ -44,17 +64,47 @@ app.controller("SkillCtrl", function($scope, SkillService) {
   $scope.skills = SkillService.companySkills();
 });
 
-function loadColorsFields(color) {
-  console.log(color);
-  fieldsToChangeColor.map(function(item,index) {
+
+function animateAttributeColor(item, pColor) {
+  var elem = $('.'+item.clazz);
+  if ("color:" == item.attr) {
+    animateColor(elem, pColor);
+  } else if ("background-color:" == item.attr) {
+    animateBackGround(elem, pColor);
+  } else if ("background:" == item.attr) {
+    animateBackGround(elem, pColor);
+  } else {
     if (item.attr.indexOf('#') != -1) {
       var content = item.attr;
-      $('.' + item.clazz).attr("style", content.replace('#', color));
+      $('.' + item.clazz).attr("style", content.replace(/#/g, pColor));
     } else {
-      $('.' + item.clazz).attr("style", item.attr + color);
+      $('.' + item.clazz).attr("style", item.attr + pColor);
     }
-  });
+  }
 }
+
+function restoreDefaultColor() {
+  loadColorsFields(DEFAULT_COLOR);
+}
+
+function animateColor(item, pColor) {
+  $(item).animate({
+    color: pColor
+  }, 1000);
+}
+
+function animateBackGround(item, pColor) {
+  $(item).animate({
+    backgroundColor: pColor
+  }, 1000);
+}
+
+function loadColorsFields(pColor) {
+  console.log(pColor);
+  fieldsToChangeColor.map(function(item, index) {
+    animateAttributeColor(item, pColor)
+  });
+
 
 function get_by_name(name) {
   var url = window.location.search
@@ -65,83 +115,83 @@ function get_by_name(name) {
     for (i = 0; i < params.length; i++) {
       var param_value = window.location.search.split("&")[i]
       if (name == param_value.substring(0, param_value.indexOf('='))) {
-          return param_value.substring(0, param_value.indexOf('=') + 1)
-        }
+        return param_value.substring(0, param_value.indexOf('=') + 1)
       }
     }
-    return "";
   }
+  return "";
+}
 
-  var COMPANY_TOKEN_PARAM_KEY = "c";
-  var LOAD_NAME_PARAM_KEY = "l";
-  var loadsIds = ['projectLoad', 'skillLoad', 'techLoad'];
+var COMPANY_TOKEN_PARAM_KEY = "c";
+var LOAD_NAME_PARAM_KEY = "l";
+var loadsIds = ['projectLoad', 'skillLoad', 'techLoad'];
 
-  function setLoadColor(color) {
-    console.log('chaging color to ' + color);
-    $('.loading').attr("style", "border-color: transparent " + color + " transparent #FFF");
-    $('.loading-text').attr("style", "color:" + color);
+function setLoadColor(color) {
+  console.log('chaging color to ' + color);
+  $('.loading').attr("style", "border-color: transparent " + color + " transparent #FFF");
+  $('.loading-text').attr("style", "color:" + color);
+}
+
+function showProjectLoad(show) {
+  if (show) {
+    $('.project-load').show('slow');
+  } else {
+    $('.project-load').hide('slow');
   }
+  hideDefaultProjects(show);
+}
 
-  function showProjectLoad(show) {
-    if (show) {
-      $('.project-load').show('slow');
-    } else {
-      $('.project-load').hide('slow');
-    }
-    hideDefaultProjects(show);
+function hideDefaultProjects(hide) {
+  if (hide) {
+    $('.projects-content').hide('slow');
+  } else {
+    $('.projects-content').show('slow');
   }
+}
 
-  function hideDefaultProjects(hide) {
-    if (hide) {
-      $('.projects-content').hide('slow');
-    } else {
-      $('.projects-content').show('slow');
-    }
+function showSkillLoad(show) {
+  if (show) {
+    $('.skill-load').show('slow');
+  } else {
+    $('.skill-load').hide('slow');
   }
+}
 
-    function showSkillLoad(show) {
-      if (show) {
-        $('.skill-load').show('slow');
-      } else {
-        $('.skill-load').hide('slow');
-      }
-    }
-
-    function hideDefaultSkill(hide) {
-      if (hide) {
-        $('.skillset').hide('slow');
-      } else {
-        $('.skillset').show('slow');
-      }
-    }
-
-  function showTechLoad(show) {
-    if (show) {
-      $('.tech-load').show('slow');
-    } else {
-      $('.tech-load').hide('slow');
-    }
+function hideDefaultSkill(hide) {
+  if (hide) {
+    $('.skillset').hide('slow');
+  } else {
+    $('.skillset').show('slow');
   }
+}
 
-  function hideDefaultTech(hide) {
-    if (hide) {
-      $('.tech-defaulf-content').hide('slow');
-    } else {
-      $('.tech-defaulf-content').show('slow');
-    }
+function showTechLoad(show) {
+  if (show) {
+    $('.tech-load').show('slow');
+  } else {
+    $('.tech-load').hide('slow');
   }
+}
 
-  function getColorFromCompany() {
-    company_token = getCompanyToken();
-    color = company_token.substring(company_token.length - 6);
-    setLoadColor(color);
-    hiddeDefaultFields();
+function hideDefaultTech(hide) {
+  if (hide) {
+    $('.tech-defaulf-content').hide('slow');
+  } else {
+    $('.tech-defaulf-content').show('slow');
   }
+}
 
-  function getCompanyToken() {
-    return get_by_name(COMPANY_TOKEN_PARAM_KEY);
-  }
+function getColorFromCompany() {
+  company_token = getCompanyToken();
+  color = company_token.substring(company_token.length - 6);
+  setLoadColor(color);
+  hiddeDefaultFields();
+}
 
-  function get_load_name() {
-    return get_by_name(LOAD_NAME_PARAM_KEY);
-  }
+function getCompanyToken() {
+  return get_by_name(COMPANY_TOKEN_PARAM_KEY);
+}
+
+function get_load_name() {
+  return get_by_name(LOAD_NAME_PARAM_KEY);
+}

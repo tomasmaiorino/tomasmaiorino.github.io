fb0190f432f14a088b6dbcf41e156ec46def9a67
tomasmaiorino <tomasmaiorino@gmail.com>
2016-11-28 23:42:32 -0200

    wip

--- assets/js/port.js
+++ assets/js/port.js
@@ -4,7 +4,19 @@ var SKILL_URL = "/api/v1/company/skill/";
 var DEFAULT_COLOR = "#5cb85c";
 var OWN_PROJECT_COLOR = "#3AAA64";
 
-var fieldsToChangeColor = ['email-link', 'level-bar-inner', 'tech-defaulf-content', 'label-theme', 'more-link', 'fa fa-star'];
+var fieldsToChangeColor = [
+    {clazz : "email-link-link", attr: "color:"},
+    {clazz : "level-bar-inner", attr: "background:"},
+    {clazz : "tech-defaulf-content", attr: "color:"},
+    {clazz : "label-theme", attr: "background:"},
+    {clazz : "label-success", attr: "background-color:"},
+    {clazz : "more-link", attr: "color:"},
+    {clazz : "fa-star", attr: "color:"},
+    {clazz : "fa-star-half", attr: "color:"},
+    {clazz : "contact-scroll", attr: "border:1px solid #"}
+];
+
+//var fieldsToChangeColor = ['email-link', 'level-bar-inner', 'tech-defaulf-content', 'label-theme', 'more-link', 'fa fa-star'];
 
 app.factory("SkillService", function() {
   return {
@@ -32,6 +44,18 @@ app.controller("SkillCtrl", function($scope, SkillService) {
   $scope.skills = SkillService.companySkills();
 });
 
+function loadColorsFields(color) {
+  console.log(color);
+  fieldsToChangeColor.map(function(item,index) {
+    if (item.attr.indexOf('#') != -1) {
+      var content = item.attr;
+      $('.' + item.clazz).attr("style", content.replace('#', color));
+    } else {
+      $('.' + item.clazz).attr("style", item.attr + color);
+    }
+  });
+}
+
 function get_by_name(name) {
   var url = window.location.search
   params = url.split("&")
@@ -40,24 +64,23 @@ function get_by_name(name) {
     params[0] = params[0].replace("?", "")
     for (i = 0; i < params.length; i++) {
       var param_value = window.location.search.split("&")[i]
-      if (name == param_value.substring(0, param_value.indexOf('=')) {
+      if (name == param_value.substring(0, param_value.indexOf('='))) {
           return param_value.substring(0, param_value.indexOf('=') + 1)
         }
       }
     }
-    return ""
+    return "";
   }
 
-  var = COMPANY_TOKEN_PARAM_KEY = "c"
-  var = LOAD_NAME_PARAM_KEY = "l"
-  var loadsIds = ['projectLoad', 'skillLoad', 'techLoad']
+  var COMPANY_TOKEN_PARAM_KEY = "c";
+  var LOAD_NAME_PARAM_KEY = "l";
+  var loadsIds = ['projectLoad', 'skillLoad', 'techLoad'];
 
   function setLoadColor(color) {
     console.log('chaging color to ' + color);
     $('.loading').attr("style", "border-color: transparent " + color + " transparent #FFF");
     $('.loading-text').attr("style", "color:" + color);
   }
-
 
   function showProjectLoad(show) {
     if (show) {

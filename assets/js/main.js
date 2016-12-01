a048ab9429747388b0d64e2809a92475b3191236
tomasmaiorino <tomasmaiorino@gmail.com>
2016-12-01 18:15:23 -0200

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,3 +90,234 @@ function showMoreJobs(){
   });
   $('#showMoreJobs').html(text);
 }
+
+
+//var fieldsToChangeColor = ['email-link', 'level-bar-inner', 'tech-defaulf-content', 'label-theme', 'more-link', 'fa fa-star'];
+var EXTERNAL_SERVICE_HOST = "http://127.0.0.1:3000";
+//var PROJECTS_URL = "token/:param";
+//var TECH_TAGS_URL = "tech/:param";
+var COMPANY_URL = EXTERNAL_SERVICE_HOST + "/api/v1/company/:option/:param";
+//var SKILL_URL = COMPANY_URL + "skill/:param";
+var DEFAULT_COLOR = "#5cb85c";
+var OWN_PROJECT_COLOR = "#3AAA64";
+var COMPANY_TOKEN_PARAM_KEY = "c";
+var LOAD_NAME_PARAM_KEY = "l";
+var loadsIds = ['projectLoad', 'skillLoad', 'techLoad'];
+var companyToken = '';
+var hexaReg = /[a-fA-F0-9]{6}/g
+//#00a1e4
+
+var fieldsToChangeColor = [{
+  clazz: "email-link-link",
+  attr: "color:"
+}, {
+  clazz: "tech-defaulf-content",
+  attr: "color:"
+}, {
+  clazz: "label-theme",
+  attr: "background:"
+}, {
+  clazz: "label-success",
+  attr: "background-color:"
+}, {
+  clazz: "more-link",
+  attr: "color:"
+}, {
+  clazz: "fa-star",
+  attr: "color:"
+}, {
+  clazz: "fa-star-half",
+  attr: "color:"
+}, {
+  clazz: "contact-scroll",
+  attr: "border:1px solid #;background-color: #"
+}, {
+  clazz: "level-bar-inner",
+  attr: "background-color:"
+}];
+
+var skillLoadClass = [{
+  clazz: "level-bar-inner",
+  attr: "background-color:"
+}];
+
+
+function animateAttributeColor(item, pColor) {
+  var elem = $('.'+item.clazz);
+  if ("color:" == item.attr) {
+    animateColor(elem, pColor);
+  } else if ("background-color:" == item.attr) {
+    animateBackGround(elem, pColor);
+  } else if ("background:" == item.attr) {
+    animateBackGround(elem, pColor);
+  } else {
+    if (item.attr.indexOf('#') != -1) {
+      var content = item.attr;
+      $('.' + item.clazz).attr("style", content.replace(/#/g, pColor));
+    } else {
+      $('.' + item.clazz).attr("style", item.attr + pColor);
+    }
+  }
+}
+
+function restoreDefaultColor() {
+  loadColorsFields(DEFAULT_COLOR);
+}
+
+function animateColor(item, pColor) {
+  $(item).animate({
+    color: pColor
+  }, 1000);
+}
+
+function animateBackGround(item, pColor) {
+  $(item).animate({
+    backgroundColor: pColor
+  }, 1000);
+}
+
+function loadColorsFields(pColor) {
+  if(pColor.indexOf('#') == -1) {
+    pColor = '#' + pColor
+  }
+  if (!hexaReg.test(pColor.replace('#',''))) {
+    console.log('invalid color ' + pColor);
+    return;
+  }
+  console.log(pColor);
+  fieldsToChangeColor.map(function(item, index) {
+    animateAttributeColor(item, pColor);
+  });
+}
+
+
+function getQueryParamByName(name) {
+  var param = window.location.search
+  //params = url.split("&")
+  if (param != undefined && param.length > 0) {
+    // remove ? from first result
+    param = param.replace("?", "")
+    //for (i = 0; i < param.length; i++) {
+      if (name == param.substring(0, param.indexOf('='))) {
+        return param.substring(param.indexOf('=') + 1)
+      }
+    //}
+  }
+  return "";
+}
+function processProjects(color) {
+  //setLoadColor(color);
+  showProjectLoad(true);
+  loadColorsFields(color);
+}
+
+function processTechTag(color) {
+  //setLoadColor(color);
+  hideDefaultTech(true);
+  showTechLoad(true);
+  loadColorsFields(color);
+}
+
+function processSkill(color) {
+  //setLoadColor(color);
+  hideDefaultSkill(true);
+  showSkillLoad(true);
+  loadColorsFields(color);
+}
+
+function fullRestore() {
+  setLoadColor(DEFAULT_COLOR);
+  //skills
+  showSkillLoad(false);
+  hideDefaultSkill(false);
+  //tech tags
+  showTechLoad(false);
+  hideDefaultTech(false);
+  //projects
+  showProjectLoad(false);
+  loadColorsFields(DEFAULT_COLOR);
+}
+
+function setLoadColor(pColor) {
+  if(pColor.indexOf('#') == -1) {
+    pColor = '#' + pColor
+  }
+  if (!hexaReg.test(color.replace('#',''))) {
+    console.log('invalid color ' + pColor);
+  } else {
+    console.log('chaging color to ' + pColor);
+    $('.loading').attr("style", "border-color: transparent " + pColor + " transparent #FFF");
+    $('.loading-text').attr("style", "color:" + pColor);
+  }
+}
+
+function showProjectLoad(show) {
+  if (show) {
+    $('.project-load').show('slow');
+  } else {
+    $('.project-load').hide('slow');
+  }
+  hideDefaultProjects(show);
+}
+
+function hideDefaultProjects(hide) {
+  if (hide) {
+    $('.projects-content').hide('slow');
+  } else {
+    $('.projects-content').show('slow');
+  }
+}
+
+function showSkillLoad(show) {
+  if (show) {
+    $('.skill-load').show('slow');
+  } else {
+    $('.skill-load').hide('slow');
+  }
+}
+
+function hideDefaultSkill(hide) {
+  if (hide) {
+    $('.skillset').hide('slow');
+  } else {
+    $('.skillset').show('slow');
+  }
+}
+
+function showTechLoad(show) {
+  if (show) {
+    $('.tech-load').show('slow');
+  } else {
+    $('.tech-load').hide('slow');
+  }
+}
+
+function hideDefaultTech(hide) {
+  if (hide) {
+    $('.tech-defaulf-content').hide('slow');
+  } else {
+    $('.tech-defaulf-content').show('slow');
+  }
+}
+
+function getColorFromCompany() {
+  if (companyToken != '') {
+    return companyToken.substring(companyToken.length - 6);
+    //setLoadColor(color);
+  }
+}
+
+function getCompanyToken() {
+  return getQueryParamByName(COMPANY_TOKEN_PARAM_KEY);
+}
+
+function get_load_name() {
+  return getQueryParamByName(LOAD_NAME_PARAM_KEY);
+}
+function initConfig() {
+  companyToken = getCompanyToken();
+  console.log('setting load color.')
+  setLoadColor(getColorFromCompany());
+  console.log('load coloer fields.')
+  loadColorsFields(getColorFromCompany());
+}
--- assets/js/port.js
+++ assets/js/port.js
@@ -1,249 +1,38 @@
-//var fieldsToChangeColor = ['email-link', 'level-bar-inner', 'tech-defaulf-content', 'label-theme', 'more-link', 'fa fa-star'];
-var EXTERNAL_SERVICE_HOST = "http://localhost:3000/";
-var SKILL_URL = "/api/v1/company/skill/";
-var PROJECTS_URL = "/api/v1/company/token/";
-var TECH_TAGS_URL = "/api/v1/company/tech/";
-var DEFAULT_COLOR = "#5cb85c";
-var OWN_PROJECT_COLOR = "#3AAA64";
-var COMPANY_TOKEN_PARAM_KEY = "c";
-var LOAD_NAME_PARAM_KEY = "l";
-var loadsIds = ['projectLoad', 'skillLoad', 'techLoad'];
-var companyToken = '';
-var hexaReg = /[a-fA-F0-9]{6}/g
+var app = angular.module("Portfolio", ['ngResource']);
 
-var app = angular.module("Portfolio", []);
+app.factory('skillService', ['$resource', function($resource) {
+  console.log("COMPANY_URL: " + COMPANY_URL);
+  var Skill = $resource(COMPANY_URL, {option:'skill', param: '@companyToken'});
+   return function(cmp) {
+     // does the external call
+     console.log("Calling skill service for company token: " + cmp);
+     return Skill.get({param: cmp});
+   };
+ }]);
 
-app.factory("SkillService", function() {
-  return {
-    companySkills: function() {
-      companyToken = getCompanyToken();
+ app.controller('SkillCtrl', ['$scope', 'skillService', function($scope, skillService) {
+   skills = skillService(companyToken);
+ }]);
 
-      var skillUrl = EXTERNAL_SERVICE_HOST + SKILL_URL + companyToken;
-      console.log("Calling skill url: " + skillUrl);
-      $http.get(skillUrl);
-      success(function(data, status, headers, config) {
-        return data.skills;
-      }).
-      error(function(data, status, headers, config) {
-        hideDefaultSkill(false);
-        showSkillLoad(false);
-        return ""
-      });
-      return users;
-    }
-  };
-});
-
-app.controller("SkillCtrl", function($scope, SkillService) {
-  $scope.skills = SkillService.companySkills();
-});
-
-//#00a1e4
-
-var fieldsToChangeColor = [{
-  clazz: "email-link-link",
-  attr: "color:"
-}, {
-  clazz: "tech-defaulf-content",
-  attr: "color:"
-}, {
-  clazz: "label-theme",
-  attr: "background:"
-}, {
-  clazz: "label-success",
-  attr: "background-color:"
-}, {
-  clazz: "more-link",
-  attr: "color:"
-}, {
-  clazz: "fa-star",
-  attr: "color:"
-}, {
-  clazz: "fa-star-half",
-  attr: "color:"
-}, {
-  clazz: "contact-scroll",
-  attr: "border:1px solid #;background-color: #"
-}, {
-  clazz: "level-bar-inner",
-  attr: "background-color:"
-}];
-
-var skillLoadClass = [{
-  clazz: "level-bar-inner",
-  attr: "background-color:"
-}];
-
-
-function animateAttributeColor(item, pColor) {
-  var elem = $('.'+item.clazz);
-  if ("color:" == item.attr) {
-    animateColor(elem, pColor);
-  } else if ("background-color:" == item.attr) {
-    animateBackGround(elem, pColor);
-  } else if ("background:" == item.attr) {
-    animateBackGround(elem, pColor);
-  } else {
-    if (item.attr.indexOf('#') != -1) {
-      var content = item.attr;
-      $('.' + item.clazz).attr("style", content.replace(/#/g, pColor));
-    } else {
-      $('.' + item.clazz).attr("style", item.attr + pColor);
-    }
-  }
-}
-
-function restoreDefaultColor() {
-  loadColorsFields(DEFAULT_COLOR);
-}
-
-function animateColor(item, pColor) {
-  $(item).animate({
-    color: pColor
-  }, 1000);
-}
-
-function animateBackGround(item, pColor) {
-  $(item).animate({
-    backgroundColor: pColor
-  }, 1000);
-}
-
-function loadColorsFields(pColor) {
-  if(pColor.indexOf('#') == -1) {
-    pColor = '#' + pColor
-  }
-  if (!hexaReg.test(pColor.replace('#',''))) {
-    console.log('invalid color ' + pColor);
-    return;
-  }
-  console.log(pColor);
-  fieldsToChangeColor.map(function(item, index) {
-    animateAttributeColor(item, pColor);
-  });
-}
-
-
-function getQueryParamByName(name) {
-  var param = window.location.search
-  //params = url.split("&")
-  if (param != undefined && param.length > 0) {
-    // remove ? from first result
-    param = param.replace("?", "")
-    //for (i = 0; i < param.length; i++) {
-      if (name == param.substring(0, param.indexOf('='))) {
-        return param.substring(param.indexOf('=') + 1)
+/*
+ angular.
+ module('myServiceModule', []).
+  controller('MyController', ['$scope', 'notify', function($scope, notify) {
+    $scope.callNotify = function(msg) {
+      notify(msg);
+    };
+  }]).
+ factory('notify', ['$window', function(win) {
+    var msgs = [];
+    return function(msg) {
+      msgs.push(msg);
+      if (msgs.length === 3) {
+        win.alert(msgs.join('\n'));
+        msgs = [];
       }
-    //}
-  }
-  return "";
-}
-function processProjects(color) {
-  setLoadColor(color);
-  showProjectLoad(true);
-  loadColorsFields(color);
-}
-
-function processTechTag(color) {
-  setLoadColor(color);
-  hideDefaultTech(true);
-  showTechLoad(true);
-  loadColorsFields(color);
-}
-
-function processSkill(color) {
-  setLoadColor(color);
-  hideDefaultSkill(true);
-  showSkillLoad(true);
-  loadColorsFields(color);
-}
-
-function fullRestore() {
-  setLoadColor(DEFAULT_COLOR);
-  //skills
-  showSkillLoad(false);
-  hideDefaultSkill(false);
-  //tech tags
-  showTechLoad(false);
-  hideDefaultTech(false);
-  //projects
-  showProjectLoad(false);
-  loadColorsFields(DEFAULT_COLOR);
-}
-
-function setLoadColor(pColor) {
-  if(pColor.indexOf('#') == -1) {
-    pColor = '#' + pColor
-  }
-  if (!hexaReg.test(color.replace('#',''))) {
-    console.log('invalid color ' + pColor);
-  } else {
-    console.log('chaging color to ' + pColor);
-    $('.loading').attr("style", "border-color: transparent " + pColor + " transparent #FFF");
-    $('.loading-text').attr("style", "color:" + pColor);
-  }
-}
-
-function showProjectLoad(show) {
-  if (show) {
-    $('.project-load').show('slow');
-  } else {
-    $('.project-load').hide('slow');
-  }
-  hideDefaultProjects(show);
-}
-
-function hideDefaultProjects(hide) {
-  if (hide) {
-    $('.projects-content').hide('slow');
-  } else {
-    $('.projects-content').show('slow');
-  }
-}
-
-function showSkillLoad(show) {
-  if (show) {
-    $('.skill-load').show('slow');
-  } else {
-    $('.skill-load').hide('slow');
-  }
-}
-
-function hideDefaultSkill(hide) {
-  if (hide) {
-    $('.skillset').hide('slow');
-  } else {
-    $('.skillset').show('slow');
-  }
-}
-
-function showTechLoad(show) {
-  if (show) {
-    $('.tech-load').show('slow');
-  } else {
-    $('.tech-load').hide('slow');
-  }
-}
-
-function hideDefaultTech(hide) {
-  if (hide) {
-    $('.tech-defaulf-content').hide('slow');
-  } else {
-    $('.tech-defaulf-content').show('slow');
-  }
-}
-
-function getColorFromCompany() {
-  if (companyToken != '') {
-    return companyToken.substring(companyToken.length - 6);
-    //setLoadColor(color);
-  }
-}
-
-function getCompanyToken() {
-  return getQueryParamByName(COMPANY_TOKEN_PARAM_KEY);
-}
-
-function get_load_name() {
-  return getQueryParamByName(LOAD_NAME_PARAM_KEY);
-}
+    };
+  }]);
+*/
+//app.controller("SkillCtrl",['SkillService', function($scope, SkillService) {
+  //$scope.skills = SkillService.companySkills();
+//}]);

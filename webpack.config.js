142508c39b9f6833cec7ce10d9e7996cc53c0837
tomasmaiorino <tomasmaiorino@gmail.com>
2019-08-28 00:13:56 +0100

    code refactor, add webpack build, webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,48 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  module: {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: ['./assets/scss/devresume.scss'],
+    output: {
+      path: path.resolve(__dirname, 'assets'),
+    },
+    module: {
     rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // fallback to style-loader in development
+			{
+				test: /\.scss$/,
+				use: [
           process.env.NODE_ENV !== 'production'
-            ? 'style-loader'
-            : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-    ],
+            ?
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'css/[name].css',
+              }
+            }
+            : MiniCssExtractPlugin.loader
+          /*
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'css/[name].blocks.css',
+						}
+          },
+          */
+					,{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'css-loader?-url'
+					},
+					{
+						loader: 'postcss-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -25,105 +52,5 @@ module.exports = {
       filename: '[name].css',
       chunkFilename: '[id].css',
     }),
-  ],
+  ]
 };
-
-/*
-module.exports = {
-  entry: ['./assets/scss/devresume.scss'],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          {
-            loader: 'sass-loader'
-          }
-          // // Compiles Sass to CSS
-          // 'sass-loader'
-          // {
-          //   loader: 'file-loader',
-          //   options: {
-          //     name: 'css/[name].blocks.css',
-          //   }
-          // }
-        ],
-      },
-    ],
-  },
-};
-*/
-
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const path = require('path');
-
-// module.exports = {
-//   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-//   entry: ['./assets/scss/devresume.scss'],
-//   output: {
-//     path: path.resolve(__dirname, 'assets'),
-//     filename: 'js/editor.blocks.js',
-//   },
-//   module: {
-//     rules: [
-// //      {
-//       //   test: /\.s[ac]ss$/i,
-//       //   use: [
-//       //     // Creates `style` nodes from JS strings
-//       //     'style-loader',
-//       //     // Translates CSS into CommonJS
-//       //     'css-loader',
-//       //     // Compiles Sass to CSS
-//       //     'sass-loader',
-//       //     {
-//       //       loader: 'file-loader',
-//       //       options: {
-//       //         name: 'css/[name].blocks.css',
-//       //       }
-//       //     }
-//       //   ],
-//       // },
-//       {
-//         test: /\.scss$/,
-//         use: ExtractTextPlugin.extract({
-//           use: [
-//             {
-//               loader: 'file-loader',
-//               options: {
-//                 name: 'css/[name].blocks.css',
-//               }
-//             },
-//             {
-//               loader: 'extract-loader'
-//             },
-//             {
-//               loader: 'css-loader?-url',
-//               options: {
-//                  minimize: true
-//               }
-//             },
-//             {
-//               loader: 'postcss-loader'
-//             },
-//           {
-//             loader: 'sass-loader',
-//             options: { minimize: true }
-//           }]
-//         }),
-//       // 	// use: [
-//       // 	// ,
-//       // 	// 	{
-//       //   //     loader: 'sass-loader',
-//       //   //     options: {
-//       //   //       minimize: true
-//       //   //     }
-//       // 	// 	}
-//       // 	// ]
-//       }
-//     ]
-//   }
-// };
